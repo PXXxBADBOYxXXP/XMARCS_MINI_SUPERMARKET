4e5940b16ea3e50661fd60ba7da6efe9c512b1cc
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2024-05-19 23:59:13 -0600

    SUBIR AL PROYECTO

---
+++ db/Conexion.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.marcobolaños.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author MARCO
+ */
+public class Conexion {
+
+    private Connection conexion;
+    private static Conexion instancia;
+
+    public Conexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/DBKinalExpressIN5BM?useSSL=false", "root", "abc123**");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Conexion getInstance() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+}
